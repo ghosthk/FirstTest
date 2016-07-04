4ffaa9b4caa23c20c455282821dbc94c386c005c
Liu Gehui <liugehui@zepplabs.com>
2016-07-04 11:37:57 +0800

    first commit

---
+++ TestGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGitHub
+//
+//  Created by Ghost on 4/7/2016.
+//  Copyright © 2016 ghost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGitHub
+//
+//  Created by Ghost on 4/7/2016.
+//  Copyright © 2016 ghost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
